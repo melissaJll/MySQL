e19a3d5db467929f942711e2b12d347ea8f0da39
melissa julia <melissa.lequipe@gmail.com>
2022-07-26 21:39:20 -0300

    Update da atividade

---
+++ Aula02/AtividadeConstruçao/exercicio01.sql
@@ -0,0 +1,12 @@
+create database db_construindo_vidas;
+use db_construindo_vidas;
+
+create table tb_categorias(
+id bigint auto_increment,
+valor double,
+Tamanho varchar,
+primary key(id)
+);
+select*from tb_categorias;
+ 
+
